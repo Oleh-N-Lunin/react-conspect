1293def8b211d05a88c0337e5ed19221b3f1c15b
Oleh-N-Lunin <67375261+Oleh-N-Lunin@users.noreply.github.com>
2021-07-16 15:15:50 +0300

    Forms

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from "react";
 
 // Components
-import Counter from './Components/Counter/Counter'
-import Toggle from "./Components/Toggle/Toggle";
+// import Counter from './Components/Counter/Counter'
+// import Toggle from "./Components/Toggle/Toggle";
+import SignUpForm from "./Components/SignUpForm/SignUpForm";
 
 function App() {
   return (
     <div className="App">
-      <Counter initialValue={10} />
-      <Toggle/>
-        <button onClick={e=> console.log(e)}>Click me!</button>
+      {/* <Counter initialValue={10} />
+      <Toggle /> */}
+      <SignUpForm/>
+        {/* <button onClick={e=> console.log(e)}>Click me!</button> */}
     </div>
   );
 }
